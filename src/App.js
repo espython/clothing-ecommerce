2b4ecc9a57d07d6f70edf26c60ae7dedeec683a9
espython <espython85@gmail.com>
2019-10-06 22:32:46 +0200

    add 1st component and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import HomePage from './HomePage';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
   }
 }
 
